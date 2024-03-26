6fa4096b0c3907bfc877f805392ef699313604eb
Phan Minh Cuong <xitrumvndn5@gmail.com>
2024-03-26 21:42:23 +0700

    Add First

---
+++ routes/auth.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const { check, body } = require("express-validator");
+
+const authController = require("../controllers/auth");
+const User = require("../models/user");
+
+const router = express.Router();
+
+router.get("/login", authController.getLogin);
+
+router.get("/signup", authController.getSignup);
+
+router.post(
+    "/login",
+    [
+        body("email")
+            .isEmail()
+            .withMessage("Please enter a valid email address.")
+            .normalizeEmail(),
+        body("password", "Password has to be valid.")
+            .isLength({ min: 5 })
+            .isAlphanumeric()
+            .trim(),
+    ],
+    authController.postLogin
+);
+
+router.post(
+    "/signup",
+    [
+        check("email")
+            .isEmail()
+            .withMessage("Please enter a valid email.")
+            .custom((value, { req }) => {
+                // if (value === "test@test.com") {
+                //     throw new Error("This email address if forbidden.");
+                // }
+                // return true;
+                return User.findOne({ email: value }).then((userDoc) => {
+                    if (userDoc) {
+                        return Promise.reject(
+                            "E-mail exists already, please pick a different one."
+                        );
+                    }
+                });
+            })
+            .normalizeEmail(),
+        // check("password"),
+        body(
+            "password",
+            "Please enter a password with only numbers and text and at least 5 characters."
+        )
+            .isLength({ min: 5 })
+            // .withMessage(
+            //     "Please enter a password with only numbers and text and at least 5 characters."
+            // )
+            .isAlphanumeric()
+            .trim(),
+        // .withMessage(
+        //     "Please enter a password with only numbers and text and at least 5 characters."
+        // )
+        body("confirmPassword")
+            .trim()
+            .custom((value, { req }) => {
+                if (value !== req.body.password) {
+                    throw new Error("Passwords have to match");
+                }
+                return true;
+            }),
+    ],
+    authController.postSignup
+);
+
+router.post("/logout", authController.postLogout);
+
+module.exports = router;
